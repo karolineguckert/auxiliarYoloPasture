5c996eb83b57be27ae1511c44be8634fc9eef75c
karolineguckert <karolinesouzaguckert@gmail.com>
2024-02-21 00:00:50 -0300

    adjusting methods

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.downloader.MenuDownload import MenuDownload
+from src.label_remover.LabelRemover import LabelRemover
 
 if __name__ == '__main__':
     # MenuDownload().download_all_folders()
-    MenuDownload().download_one_folder_by_user()
+    # MenuDownload().download_one_folder_by_user()
+    LabelRemover().remove_empty_file_label()
 
--- src/label_remover/LabelRemover.py
+++ src/label_remover/LabelRemover.py
@@ -1,3 +1,40 @@
+import os
+
+
 class LabelRemover:
     def __init__(self):
-        print("a")
+        self.ROOT_PATH = './images'
+
+    def remove_empty_file_label(self):
+        file_list = os.listdir(self.ROOT_PATH)
+
+        for file in file_list:
+            labels_folder_path = '{}/{}/labels'.format(self.ROOT_PATH, file)
+            labels_list = os.listdir(labels_folder_path)
+
+            for label in labels_list:
+                label_path = '{}/{}'.format(labels_folder_path, label)
+                file = open(label_path, "r")
+                content = file.read()
+
+                is_file_empty = len(content) == 0
+                file.close()
+
+                if is_file_empty:
+                    os.remove(label_path)
+
+
+    def remove_invalid_bounding_box(self):
+        file_list = os.listdir(self.ROOT_PATH)
+
+        for file in file_list:
+            labels_folder_path = '{}/{}/labels'.format(self.ROOT_PATH, file)
+            labels_list = os.listdir(labels_folder_path)
+
+            for label in labels_list:
+                label_path = '{}/{}'.format(labels_folder_path, label)
+                file = open(label_path, "w+")
+                content = file.read()
+                file.close()
+
+
