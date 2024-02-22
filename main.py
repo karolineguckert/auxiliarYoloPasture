6599ee8bd78764ca696775e30dca252c13adce2e
karolineguckert <karolinesouzaguckert@gmail.com>
2024-02-22 00:52:30 -0300

    adding new methods

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from src.downloader.MenuDownload import MenuDownload
 from src.label_remover.LabelRemover import LabelRemover
+from src.folder_organizer.FolderOrganizer import FolderOrganizer
+
 
 if __name__ == '__main__':
     # MenuDownload().download_all_folders()
     # MenuDownload().download_one_folder_by_user()
     # LabelRemover().remove_invalid_bounding_box()
-    LabelRemover().remove_empty_file_label()
-
+    # LabelRemover().remove_empty_file_label()
+    # FolderOrganizer().move_images_to_train()
+    FolderOrganizer().move_images_to_detect()
--- src/folder_organizer/FolderOrganizer.py
+++ src/folder_organizer/FolderOrganizer.py
@@ -1,16 +1,36 @@
 import os
+import shutil
 
 
 class FolderOrganizer:
     def __init__(self):
         self.ROOT_PATH = './images'
 
-    def move_images(self):
+    def move_images_to_train(self):
         file_list = os.listdir(self.ROOT_PATH)
 
         for file in file_list:
             labels_folder_path = '{}/{}/labels'.format(self.ROOT_PATH, file)
             labels_list = os.listdir(labels_folder_path)
 
+            images_folder_path = '{}/{}/images'.format(self.ROOT_PATH, file)
+
             for label in labels_list:
                 label_path = '{}/{}'.format(labels_folder_path, label)
+
+                image_name = label.replace(".txt", ".jpg")
+                image_path = '{}/{}'.format(images_folder_path, image_name)
+
+                shutil.move(label_path, 'images_to_train/labels/{}'.format(label))
+                shutil.move(image_path, 'images_to_train/images/{}'.format(image_name))
+
+    def move_images_to_detect(self):
+        file_list = os.listdir(self.ROOT_PATH)
+
+        for file in file_list:
+            images_folder_path = '{}/{}/images'.format(self.ROOT_PATH, file)
+            images_list = os.listdir(images_folder_path)
+
+            for image in images_list:
+                image_path = '{}/{}'.format(images_folder_path, image)
+                shutil.move(image_path, 'image_to_detect/{}'.format(image))
