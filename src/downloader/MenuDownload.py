3226e6c417ef2adcf1228b86a2ce422c5163b04f
karolineguckert <karolinesouzaguckert@gmail.com>
2024-03-16 18:17:51 -0300

    commenting methods

--- src/downloader/MenuDownload.py
+++ src/downloader/MenuDownload.py
@@ -3,7 +3,7 @@ from src.enums.Projects import Project
 
 
 class MenuDownload:
-    def __init__(self):
+    def __init__(self, ip, port, token):
         self.CANCELED = None
         self.YES = "Y"
         self.OPTIONS = [
@@ -17,7 +17,12 @@ class MenuDownload:
             "CAMPO_4_SUBIDA",
             "CAMPO_4_CIMA"
         ]
+        self.IP = ip
+        self.PORT = port
+        self.TOKEN = token
 
+    # Assistant method to crete menu options
+    #
     def __create_options(self):
         menu_options = "";
         for i in range(len(self.OPTIONS)):
@@ -25,6 +30,8 @@ class MenuDownload:
 
         return menu_options
 
+    # Assistant method to crete the complete menu
+    #
     def __create_menu_download(self):
         menu = "---- CHOOSE WHICH PROJECT DO YOU WANT TO DOWNLOAD ----\n"
         menu += self.__create_options()
@@ -39,6 +46,8 @@ class MenuDownload:
 
         return self.CANCELED
 
+    # Assistant method to download one folder without using menu method
+    #
     def download_one_folder_by_user(self):
         project = self.__create_menu_download()
 
@@ -48,9 +57,14 @@ class MenuDownload:
             final_image_id = project[2]
             name_main_folder = project[3]
 
-            DownloaderOfImages().get_all_images(project_number, "YOLO", initial_image_id, final_image_id,
-                                                name_main_folder)
+            DownloaderOfImages(self.IP, self.PORT, self.TOKEN).get_all_images(project_number,
+                                                                              "YOLO",
+                                                                              initial_image_id,
+                                                                              final_image_id,
+                                                                              name_main_folder)
 
+    # Assistant method to download one folder using menu method
+    #
     def __download_one_folder(self, project):
         if project is not None:
             project_number = project[0]
@@ -58,13 +72,20 @@ class MenuDownload:
             final_image_id = project[2]
             name_main_folder = project[3]
 
-            DownloaderOfImages().get_all_images(project_number, "YOLO", initial_image_id, final_image_id,
-                                                name_main_folder)
+            DownloaderOfImages(self.IP, self.PORT, self.TOKEN).get_all_images(project_number,
+                                                                              "YOLO",
+                                                                              initial_image_id,
+                                                                              final_image_id,
+                                                                              name_main_folder)
 
+    # Assistant method to get the respective folder by the option select in the menu
+    #
     def __get_project_by_option_answer(self, option_answer):
         option_name = self.OPTIONS[int(option_answer) - 1]
         return Project.get_project(option_name)
 
+    # Assistant method to download all folders without using menu method
+    #
     def download_all_folders(self):
         for i in range(1, len(self.OPTIONS) + 1):
             project = self.__get_project_by_option_answer(i)
