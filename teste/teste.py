defbbf3264738307f0b5cb059786fc076a96734d
karolineguckert <karolinesouzaguckert@gmail.com>
2024-05-27 20:52:22 -0300

    removing not used file

--- teste/teste.py
+++
@@ -1,7 +0,0 @@
-# from label_studio_converter import Converter
-#
-# c = Converter('ls-tasks.label_config.xml')
-# c.convert_to_json('examples/sentiment_analysis/completions/', 'tmp/output.json')
-#
-# label-studio-converter import yolo -i C:\Users\karol\Desktop\YOLO\auxiliarYoloPasture/images/CAMPO_1_CIMA -o ls-tasks.json --out-type annotations
-# ls-tasks.label_config
